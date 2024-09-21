38a0d6e7ac1ea8053d221151f7e7e6046a8a98cb
Derek Hayes <derek2099@gmail.com>
2024-09-21 16:45:32 -0500

    Change background color on mouseover

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
 const container = document.querySelector("#container");
 
+
 for (let i = 0; i < 16; i++) {
     const div = document.createElement("div");
+    div.classList.add("divs");
     container.appendChild(div);
 }
+
+const divs = container.querySelectorAll(".divs");
+
+divs.forEach(element => element.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "red";
+}))
