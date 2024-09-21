0523d081cf160d012d6b9ecef349e00dfc14c152
Derek Hayes <derek2099@gmail.com>
2024-09-21 18:21:56 -0500

    Keep internal divs inside container
    Works ok for  a fixed container size. I think there's a better way.

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,36 @@
 const container = document.querySelector("#container");
+const btn = document.querySelector("button");
+let sides = 16;
 
+// create the divs
+function createSquares(sides) {
+    container.innerHTML = '';
 
-for (let i = 0; i < 16; i++) {
-    const div = document.createElement("div");
-    div.classList.add("divs");
-    container.appendChild(div);
+    let totalDivs = sides * sides;
+    let containerSize = container.clientWidth;
+    let squareSize = containerSize / sides;
+
+    for (let i = 0; i < totalDivs; i++) {
+        const div = document.createElement("div");
+        div.classList.add("divs");
+        div.style.width = `${squareSize}px`;
+        div.style.height = `${squareSize}px`;
+        container.appendChild(div);
+    }
+
+    // create the nodeList
+    const divs = container.querySelectorAll(".divs");
+
+    // change color on mouseover
+    divs.forEach(element => element.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "red";
+    }));
 }
 
-const divs = container.querySelectorAll(".divs");
+createSquares(sides);
 
-divs.forEach(element => element.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "red";
-}))
+// get user input
+btn.addEventListener("click", (e) => {
+    sides = prompt("How many squares per side would you like?", sides);
+    createSquares(sides);
+});
